766a914f4df0467d2824c768cb35e1cb5143b35a
tnagler <thomas.nagler@tum.de>
2017-04-01 00:22:09 +0200

    add package description to manuals

---
+++ R/rvinecopulib.R
@@ -0,0 +1,14 @@
+#' High Performance Algorithms for Vine Copula Modeling
+#'
+#' @name rvinecopulib
+#' @docType package
+#' @useDynLib rvinecopulib
+#' @importFrom Rcpp evalCpp
+#'
+#' @author Thomas Nagler, Thibault Vatter
+#
+#' @keywords package
+#'
+#' @examples
+#'
+NULL
