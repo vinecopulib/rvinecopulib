fb3983a0439d25f75159179c1800903e552cf3b5
tnagler <thomas.nagler@tum.de>
2017-03-31 23:28:22 +0200

    make bash script include RcppEigen header in tools_interfaces

--- src/update_vinecopulib.sh
+++ src/update_vinecopulib.sh
@@ -11,6 +11,7 @@ mv ./vinecopulib/src/* .
 mv ./vinecopulib/include ../inst
 
 sed -i '11i#define INTERFACED_FROM_R' ./../inst/include/vinecopulib/misc/tools_interface.hpp
+sed -i '12i#include <RcppEigen.h>' ./../inst/include/vinecopulib/misc/tools_interface.hpp
 
 rm -rf ./../inst/include/mainpage.h
 rm -rf vinecopulib
