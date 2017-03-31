0a2cc58732e8127ba267e9435d8960f5951be50c
tnagler <thomas.nagler@tum.de>
2017-03-31 23:41:50 +0200

    add class bicop_dist

---
+++ R/bicop_dist.R
@@ -0,0 +1,46 @@
+#' Bivariate copula distributions
+#'
+#' A bivariate copula distribution is specified by:
+#'
+#' @param family the copula family, a string containing the family name (see
+#' *Details* for all possible families).
+#' @param rotation the rotation of the copula, one of `0`, `90`, `180` `270`.
+#' @param parameters a vector or matrix of copula paramters.
+#'
+#' @details
+#' The implemented families are:
+#' \describe{
+#' \item{`indep`}{Independence copula.}
+#' \item{`gaussian`}{Gaussian copula.}
+#' \item{`t`}{Student t copula.}
+#' \item{`clayton`}{Clayton copula.}
+#' \item{`gumbel`}{Gumbel copula.}
+#' \item{`frank`}{Frank copula.}
+#' \item{`Joe`}{Joe copula.}
+#' \item{`bb1`}{BB1 copula.}
+#' \item{`bb6`}{BB6 copula.}
+#' \item{`bb7`}{BB7 copula.}
+#' \item{`bb8`}{BB8 copula.}
+#' }
+#'
+#' @return An object of class `bicop_dist`.
+#'
+#' @examples
+#' bicop_dist("clayton", 0, 3)
+#'
+#' str(bicop_dist("clayton", 0, 3))
+#'
+#' @export
+bicop_dist <- function(family = "indep", rotation = 0, parameters = numeric(0)) {
+    lst <- list(family = family, rotation = rotation, parameters = parameters)
+    structure(lst, class = "bicop_dist")
+}
+
+#' @export
+print.bicop_dist <- function(x, ...) {
+    cat("Bivariate copula ('bicop_dist'): ",
+        "family = ", x$family,
+        ", rotation = ", x$rotation,
+        ", parameters = ", x$parameters,
+        sep = "")
+}
