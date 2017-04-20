8ffbef7fa7ce424e43d9b2b1119dde149697ce79
tnagler <thomas.nagler@tum.de>
2017-04-20 23:21:34 +0200

    add d/r/hx/hix functions

--- R/bicop_dist.R
+++ R/bicop_dist.R
@@ -6,9 +6,14 @@
 #' *Details* for all possible families).
 #' @param rotation the rotation of the copula, one of `0`, `90`, `180` `270`.
 #' @param parameters a vector or matrix of copula paramters.
+#' 
+#' @note 
+#' The evaluation functions can optionally be used with a `bicop_dist` object, 
+#' e.g., `dbicop(c(0.1, 0.5), bicop_dist("indep"))`.
 #'
 #' @details
-#' The implemented families are:
+#' The implemented families listed below. Partial matching is activated, i.e., 
+#' `"gauss"` is equivalent to `"gaussian"`.
 #' \describe{
 #' \item{`indep`}{Independence copula.}
 #' \item{`gaussian`}{Gaussian copula.}
@@ -22,20 +27,21 @@
 #' \item{`bb7`}{BB7 copula.}
 #' \item{`bb8`}{BB8 copula.}
 #' }
+#' 
 #'
 #' @return An object of class `bicop_dist`.
 #'
 #' @examples
-#' bicop_dist("clayton", 0, 3)
-#'
-#' str(bicop_dist("clayton", 0, 3))
-#'
+#' bicop_dist("gaussian", 0, 3)
+#' str(bicop_dist("gauss", 0, 3))
+#' 
+#' bicop <- bicop_dist("clayton", 90, 3)
 #' @export
 bicop_dist <- function(family = "indep", rotation = 0, parameters = numeric(0)) {
     stopifnot(length(family) == 1)
     if (family == "t")
         family <- "student"
-    family <- all_families[pmatch(family, all_families)]
+    family <- family_set_all[pmatch(family, family_set_all)]
     dist <- list(family = family,
                  rotation = rotation,
                  parameters = as.matrix(parameters))
@@ -52,8 +58,69 @@ print.bicop_dist <- function(x, ...) {
         sep = "")
 }
 
-all_families <- c(
-    "indep", 
-    "gaussian", "clayton", "gumbel", "frank", "joe", 
-    "bb1", "bb6", "bb7", "bb8"
-)
+#' @rdname bicop_dist
+#' @examples
+#' # evaluate the copula density
+#' dbicop(c(0.1, 0.2), "clay", 90, 3)
+#' dbicop(c(0.1, 0.2), bicop)
+#' 
+#' @export
+dbicop <- function(u, family, rotation, parameters) {
+    bicop <- args2bicop(as.list(environment()))
+    bicop_pdf_cpp(if_vec_to_matrix(u), bicop)
+}
+
+#' @rdname bicop_dist
+#' @param n number of observations. If `length(n) > 1``, the length is taken to 
+#'   be the number required.
+#' @examples
+#' # evaluate the copula density
+#' plot(rbicop(500, "clay", 90, 3))
+#' plot(rbicop(500, bicop))
+#' 
+#' @export
+rbicop <- function(n, family, rotation, parameters) {
+    if (length(n) > 1)
+        n <- length(n)
+    bicop <- args2bicop(as.list(environment()))
+    bicop_simulate_cpp(n, bicop)
+}
+
+#' @rdname bicop_dist
+#' @examples
+#' # evaluate the copula density
+#' h1bicop(c(0.1, 0.2), "frank", 0, 5)
+#' h2bicop(c(0.1, 0.2), bicop)
+#' 
+#' @export
+h1bicop <- function(u, family, rotation, parameters) {
+    bicop <- args2bicop(as.list(environment()))
+    bicop_hfunc1_cpp(if_vec_to_matrix(u), bicop)
+}
+
+#' @rdname bicop_dist
+#' @export
+h2bicop <- function(u, family, rotation, parameters) {
+    bicop <- args2bicop(as.list(environment()))
+    bicop_hfunc2_cpp(if_vec_to_matrix(u), bicop)
+}
+
+#' @rdname bicop_dist
+#' @examples
+#' # evaluate the copula density
+#' hi1bicop(c(0.1, 0.2), "bb6", 180, 5)
+#' hi2bicop(c(0.1, 0.2), bicop)
+#' 
+#' @export
+hi1bicop <- function(u, family, rotation, parameters) {
+    bicop <- args2bicop(as.list(environment()))
+    bicop_hinv1_cpp(if_vec_to_matrix(u), bicop)
+}
+
+#' @rdname bicop_dist
+#' @export
+hi2bicop <- function(u, family, rotation, parameters) {
+    bicop <- args2bicop(as.list(environment()))
+    bicop_hinv2_cpp(if_vec_to_matrix(u), bicop)
+}
+
--- tests/testthat/test_bicop_dist.R
+++ tests/testthat/test_bicop_dist.R
@@ -18,3 +18,14 @@ test_that("partial matching for family names", {
     bicop_dist("ind")
     bicop_dist("gauss") 
 })
+
+test_that("d/r/hx/hix functions work", {
+    dist <- bicop_dist("bb1", 270, c(1, 2))
+    u <- rbicop(500, "bb1", 270, c(1, 2))
+    expect_gte(min(dbicop(c(0.1, 0.2), dist)), 0)
+    expect_gte(min(dbicop(u, dist)), 0)
+    expect_lte(max(h1bicop(c(0.1, 0.2), dist)), 1)
+    expect_lte(max(h2bicop(u, dist)), 1)
+    expect_lte(max(hi1bicop(c(0.1, 0.2), dist)), 1)
+    expect_lte(max(hi2bicop(u, dist)), 1)
+})
