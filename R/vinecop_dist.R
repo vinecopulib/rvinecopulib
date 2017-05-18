5fa850ffe7411d8628d64435dcec5c51773e34f6
tnagler <thomas.nagler@tum.de>
2017-05-18 15:24:36 +0200

    add example code for vinecop_dist

--- R/vinecop_dist.R
+++ R/vinecop_dist.R
@@ -4,9 +4,29 @@
 #'    `pair_copulas[[t]][[e]]` corresponds to the pair-copula at edge `e` in
 #'    tree `t`.
 #' @param matrix A quadratic structure matrix of dimension 
-#'   `length(pair_copulas) + 1`. 
+#'   `length(pair_copulas) + 1. All entries in the lower right triangle must 
+#'    be zero (see *Examples*). 
 #'
 #' @examples
+#' # specify pair-copulas
+#' bicop <- bicop_dist("bb1", 90, c(3, 2))
+#' pcs <- list(
+#'     list(bicop, bicop),  # pair-copulas in first tree 
+#'     list(bicop)          # pair-copulas in second tree 
+#'  )
+#'  
+#' # specify R-vine matrix
+#' mat <- matrix(c(1, 2, 3, 1, 2, 0, 1, 0, 0), 3, 3) 
+#' 
+#' # set up vine copula model
+#' vc <- vinecop_dist(pcs, mat)
+#' 
+#' # simulate from the model
+#' u <- rvinecop(200, vc)
+#' pairs(u)
+#' 
+#' # evaluate the density
+#' dvinecop(u[1, ], vc)
 #' 
 #' @export
 vinecop_dist <- function(pair_copulas, matrix) {
