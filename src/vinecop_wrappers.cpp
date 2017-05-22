598025a43cc6dfe91b1f61170ba22bb745e32c58
tvatter <thibault.vatter@gmail.com>
2017-05-22 18:19:23 -0400

    avoid unnecessary copies in vinecop_wrap

--- src/vinecop_wrappers.cpp
+++ src/vinecop_wrappers.cpp
@@ -7,36 +7,36 @@ using namespace vinecopulib;
 Vinecop vinecop_wrap(const Rcpp::List& vinecop_r)
 {
     Rcpp::List pair_copulas = vinecop_r["pair_copulas"];
-    int d = pair_copulas.size() + 1;
+    size_t d = pair_copulas.size() + 1;
     auto pc_store = Vinecop::make_pair_copula_store(d);
     Rcpp::List tree_pcs, pc;
-    for (int t = 0; t < d - 1; ++t) {
-        for(int e = 0; e < d - 1 - t; ++e) {
-            tree_pcs = pair_copulas[t];
+    for (size_t t = 0; t < d - 1; ++t) {
+        tree_pcs = pair_copulas[t];
+        for(size_t e = 0; e < d - 1 - t; ++e) {
             pc = tree_pcs[e];
             pc_store[t][e] = bicop_wrap(pc);
         }
     }
-    
+
     Eigen::Matrix<size_t, Eigen::Dynamic, Eigen::Dynamic> matrix = vinecop_r["matrix"];
-    
+
     return Vinecop(pc_store, matrix);
 }
 
 
 Rcpp::List vinecop_wrap(Vinecop& vinecop_cpp) {
     auto matrix = vinecop_cpp.get_matrix();
-    int d = matrix.cols();
+    size_t d = matrix.cols();
     Rcpp::List pair_copulas(d - 1);
-    
-    for (int t = 0; t < d - 1; ++t) {
+
+    for (size_t t = 0; t < d - 1; ++t) {
         Rcpp::List tree_pcs(d - 1 - t);
-        for(int e = 0; e < d - 1 - t; ++e) {
+        for(size_t e = 0; e < d - 1 - t; ++e) {
             tree_pcs[e] = bicop_wrap(vinecop_cpp.get_pair_copula(t, e));
         }
         pair_copulas[t] = tree_pcs;
     }
-    
+
     return Rcpp::List::create(
         Rcpp::Named("pair_copulas") = pair_copulas,
         Rcpp::Named("matrix") = matrix
@@ -85,7 +85,7 @@ Rcpp::List vinecop_select_cpp(
     for (unsigned int fam = 0; fam < fam_set.size(); ++fam) {
         fam_set[fam] = to_cpp_family(family_set[fam]);
     }
-    
+
     FitControlsVinecop fit_controls(
             fam_set,
             method,
@@ -104,6 +104,6 @@ Rcpp::List vinecop_select_cpp(
     } else {
         vinecop_cpp.select_all(data, fit_controls);
     }
-    
+
     return vinecop_wrap(vinecop_cpp);
 }
