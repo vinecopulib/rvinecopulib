8aba9f760ae60796f6fd30deab9a990e82e8ff55
Thibault Vatter <thibault.vatter@gmail.com>
2017-06-13 18:57:19 -0400

    add bicop plots (#20)
    * add bicop plots
    
    * remove authorship (temporarily)
    
    * add unit tests and make return of plot invisible

--- tests/testthat/test_bicop_dist.R
+++ tests/testthat/test_bicop_dist.R
@@ -31,3 +31,11 @@ test_that("d/p/r/h functions work", {
     expect_lte(max(hbicop(c(0.1, 0.2), 1, dist, inverse = TRUE)), 1)
     expect_lte(max(hbicop(u, 1, dist, inverse = TRUE)), 1)
 })
+
+test_that("plot functions work", {
+    dist <- bicop_dist("gumbel", 90, 3)
+    expect_silent(plot(dist))
+    expect_silent(contour(dist))
+    expect_silent(plot(dist, margins = "norm"))
+    expect_silent(contour(dist, margins = "unif"))
+})
