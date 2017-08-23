e6c2ee6dcfcd444328f22f72db6668ce0df558fc
tvatter <thibault.vatter@gmail.com>
2017-08-23 18:16:50 -0400

    add vine copula example to rvinecopulib.R/Rd

--- R/rvinecopulib.R
+++ R/rvinecopulib.R
@@ -10,8 +10,16 @@
 #' @keywords package
 #'
 #' @examples
+#' # bivariate copula
 #' dist <- bicop_dist("clayton", 90, 3)
 #' u <- rbicop(100, dist)
 #' fit <- bicop(u)
 #' fit
+#' summary(fit)
+#' 
+#' # vine copula
+#' u <- sapply(1:3, function(i) runif(50))
+#' fit <- vinecop(u, "par")
+#' fit
+#' summary(fit)
 NULL
