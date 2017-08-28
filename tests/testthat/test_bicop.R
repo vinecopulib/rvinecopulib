b1e722256c75307b2605e2e33658a8ce1faf7a81
tvatter <thibault.vatter@gmail.com>
2017-08-28 10:50:37 -0400

    export bicop.logLik and bicop.print

--- tests/testthat/test_bicop.R
+++ tests/testthat/test_bicop.R
@@ -34,4 +34,5 @@ test_that("S3 generics work", {
     expect_equal(predict(fit, u, what = "hfunc2"), fitted(fit, what = "hfunc2"))
     expect_equal(predict(fit, u, what = "hinv1"), fitted(fit, what = "hinv1"))
     expect_equal(predict(fit, u, what = "hinv2"), fitted(fit, what = "hinv2"))
+    expect_equivalent(logLik(fit), sum(log(predict(fit, u, what = "pdf"))))
 })
