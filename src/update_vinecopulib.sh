cd34069deb96778ef2f55c6fda92ea9708397f1d
tnagler <thomas.nagler@tum.de>
2017-10-27 16:38:02 +0200

    update pull script

--- src/update_vinecopulib.sh
+++ src/update_vinecopulib.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-git clone https://github.com/vinecopulib/vinecopulib
+git clone https://github.com/vinecopulib/vinecopulib/
 
-rm -rf bicop
-rm -rf misc
-rm -rf vinecop
 rm -rf ../inst/include/vinecopulib*
-
-mv ./vinecopulib/src/* .
 mv ./vinecopulib/include/* ../inst/include
 
-gsed -i '11i#define INTERFACED_FROM_R' ./../inst/include/vinecopulib/misc/tools_interface.hpp
+sed -i '11i#define INTERFACED_FROM_R' ./../inst/include/vinecopulib/misc/tools_interface.hpp
 
 rm -rf ./../inst/include/mainpage.h
 rm -rf vinecopulib
