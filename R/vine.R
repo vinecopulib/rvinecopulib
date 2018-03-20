bcffec0a25a563df411b8add011d9b928240f16b
tvatter <thibault.vatter@gmail.com>
2018-03-20 15:01:21 -0400

    add doc and clean-up

--- R/vine.R
+++ R/vine.R
@@ -27,7 +27,29 @@
 #' `vine` and `vine_dist` for [vine()].
 #'
 #' @examples
-#' # TODO
+#' # specify pair-copulas
+#' bicop <- bicop_dist("bb1", 90, c(3, 2))
+#' pcs <- list(
+#'     list(bicop, bicop),  # pair-copulas in first tree 
+#'     list(bicop)          # pair-copulas in second tree 
+#'  )
+#'  
+#' # specify R-vine matrix
+#' mat <- matrix(c(1, 2, 3, 1, 2, 0, 1, 0, 0), 3, 3) 
+#' 
+#' # set up vine copula model with Gaussian margins
+#' vc <- vine_dist(list(name = "norm"), pcs, mat)
+#' 
+#' # show model
+#' summary(vc)
+#' 
+#' # simulate some data
+#' x <- rvine(50, vc)
+#' 
+#' # estimate a vine copula model
+#' fit <- vine(x, cop_controls = list(family_set = "par"))
+#' summary(fit)
+#' 
 #' @importFrom kde1d kde1d dkde1d pkde1d qkde1d
 #' @importFrom cctools cont_conv expand_vec
 #' @export
@@ -35,7 +57,7 @@ vine <- function(data,
                  marg_controls = list(mult = NULL, 
                                       xmin = -Inf, 
                                       xmax = Inf, 
-                                      bw = NULL), 
+                                      bw = NA), 
                  cop_controls = list(family_set = "all", 
                                      matrix = NA, 
                                      par_method = "mle", 
@@ -47,7 +69,7 @@ vine <- function(data,
                                      trunc_lvl = Inf, 
                                      tree_crit = "tau", 
                                      threshold = 0, 
-                                     keep_data = TRUE, 
+                                     keep_data = TRUE,
                                      show_trace = FALSE, 
                                      cores = 1)) {
     
@@ -55,6 +77,9 @@ vine <- function(data,
     if (NCOL(data_cc) == 1)
         stop("data must be multivariate.")
     d <- ncol(data_cc)
+    
+    if (is.null(cop_controls$keep_data))
+        cop_controls$keep_data <- TRUE
     
     ## check that the correct arguments are there
     marg_controls_names <- c("mult", "xmin", "xmax", "bw")
@@ -81,20 +106,15 @@ vine <- function(data,
     
     ## estimation of the marginals
     vine <- list()
-    i_disc <- attr(data_cc, "i_disc")
-    theta <- attr(data_cc, "theta")
-    vine$marg <- lapply(1:d, function(k) 
-        kde1d(data_cc[, k], 
-              xmin = marg_controls$xmin[k], 
-              xmax = marg_controls$xmax[k],
-              bw = marg_controls$bw[k],
-              mult = marg_controls$mult,
-              bw_min = ifelse(k %in% i_disc, 0.5 - theta, 0)))
+    vine$marg <- lapply(1:d, function(k) kde1d(data_cc[, k],
+                                               xmin = marg_controls$xmin[k], 
+                                               xmax = marg_controls$xmax[k],
+                                               bw = marg_controls$bw[k],
+                                               mult = marg_controls$mult))
     vine$marg_controls <- marg_controls
     
     ## estimation of the R-vine copula (only if d > 1)
     if (d > 1) {
-        
         ## transform to copula data
         cop_controls$data <- sapply(1:d, function(k) pkde1d(data_cc[, k],
                                                             vine$marg[[k]]))
@@ -109,13 +129,17 @@ vine <- function(data,
         ## to potentially save the data on the standard scale
         cop_controls$keep_data <- keep_data
     }
+    vine$cop_controls <- cop_controls[-which(names(cop_controls) == "data")]
     
     ## add information about the fit
-    vine$names <- colnames(data)
     if (keep_data) {
         vine$data <- data_cc
+    } else {
+        vine$data <- matrix(NA, ncol = ncol(data_cc))
+        colnames(vine$data) <- colnames(data_cc)
+        attr(vine$data, "i_disc") <- attr(data_cc, "i_disc")
+        attr(vine$data, "theta") <- attr(data_cc, "theta")
     }
-    vine$cop_controls <- cop_controls[-which(names(cop_controls) == "data")]
     vine$nobs <- nrow(data)
     
     ## create and return object
@@ -123,7 +147,7 @@ vine <- function(data,
 }
 
 #' @param marg A list with with each element containing the specification of a 
-#' marginal [stats:Distributions](distribution). Each marginal specification 
+#' marginal [stats::Distributions]. Each marginal specification 
 #' should be a list with containing at least the name and optionally the 
 #' parameters, e.g. `list(list(name = "norm"), list(name = "norm", mu = 1), list(name = "beta", shape1 = 1, shape2 = 1))`.
 #' Note that parameters that have no default values have to be provided. 
@@ -131,6 +155,10 @@ vine <- function(data,
 #' @param pair_copulas A nested list of 'bicop_dist' objects, where 
 #'    \code{pair_copulas[[t]][[e]]} corresponds to the pair-copula at edge `e` in
 #'    tree `t`.
+#' @param matrix a quadratic matrix specifying the structure matrix (see 
+#'   [check_rvine_matrix()]); for [vinecop_dist()], the dimension must be 
+#'   `length(pair_copulas)-1`; for [vinecop()], `matrix = NA` performs
+#'   automatic structure selection.
 #' @rdname vine
 #' @export
 vine_dist <- function(marg, pair_copulas, matrix) {
@@ -151,10 +179,10 @@ vine_dist <- function(marg, pair_copulas, matrix) {
                              unlist(check_marg[!is_ok]), ".", sep = "\n"))
         stop(msg)
     }
-    
+
     # create the vinecop object
-    vinecop <- vinecop(pair_copulas, matrix)
+    vinecop <- vinecop_dist(pair_copulas, matrix)
     
     # create object
-    structure(list(marg = marg, vinecop = vinecop), class = "vine_dist")
+    structure(list(marg = marg, cop = vinecop), class = "vine_dist")
 }
