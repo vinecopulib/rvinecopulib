99a03ccedad4b27aed84505a08b3b299ce75619d
tnagler <thomas.nagler@tum.de>
2019-09-06 14:25:23 +0200

    add deg argument to margins_controls

--- R/vine.R
+++ R/vine.R
@@ -59,19 +59,19 @@
 #'   list(bicop, bicop), # pair-copulas in first tree
 #'   list(bicop) # pair-copulas in second tree
 #' )
-#' 
+#'
 #' # specify R-vine matrix
 #' mat <- matrix(c(1, 2, 3, 1, 2, 0, 1, 0, 0), 3, 3)
-#' 
+#'
 #' # set up vine copula model with Gaussian margins
 #' vc <- vine_dist(list(distr = "norm"), pcs, mat)
-#' 
+#'
 #' # show model
 #' summary(vc)
-#' 
+#'
 #' # simulate some data
 #' x <- rvine(50, vc)
-#' 
+#'
 #' # estimate a vine copula model
 #' fit <- vine(x, copula_controls = list(family_set = "par"))
 #' summary(fit)
@@ -83,7 +83,8 @@ vine <- function(data,
                    mult = NULL,
                    xmin = NaN,
                    xmax = NaN,
-                   bw = NA
+                   bw = NA,
+                   deg = 2
                  ),
                  copula_controls = list(
                    family_set = "all",
@@ -110,7 +111,8 @@ vine <- function(data,
   assert_that(NCOL(data_cc) > 1, msg = "data must be multivariate.")
   d <- ncol(data_cc)
   assert_that(is.list(margins_controls))
-  assert_that(in_set(names(margins_controls), c("mult", "xmin", "xmax", "bw")))
+  allowed_margins_controls <- c("mult", "xmin", "xmax", "bw", "deg")
+  assert_that(in_set(names(margins_controls), allowed_margins_controls))
   assert_that(is.list(copula_controls))
   if (is.null(copula_controls$keep_data)) {
     copula_controls$keep_data <- TRUE
@@ -122,10 +124,11 @@ vine <- function(data,
   ## estimation of the marginals
   vine <- list()
   vine$margins <- lapply(1:d, function(k) kde1d(data_cc[, k],
+      mult = margins_controls$mult,
       xmin = margins_controls$xmin[k],
       xmax = margins_controls$xmax[k],
       bw = margins_controls$bw[k],
-      mult = margins_controls$mult
+      deg = margins_controls$deg[k]
     ))
   vine$margins_controls <- margins_controls
 
@@ -198,7 +201,7 @@ vine_dist <- function(margins, pair_copulas, structure) {
 }
 
 expand_margin_controls <- function(controls, d, data) {
-  default_controls <- list(mult = NULL, xmin = NaN, xmax = NaN, bw = NA)
+  default_controls <- list(mult = NULL, xmin = NaN, xmax = NaN, bw = NA, deg = 2)
   controls <- modifyList(default_controls, controls)
   if (is.null(controls[["mult"]])) {
     controls[["mult"]] <- log(1 + d)
