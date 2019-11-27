1d11cf794aab824249f3c9a82aa90fe22572e8b2
tnagler <thomas.nagler@tum.de>
2019-11-27 15:54:01 +0100

    export plot.rvine_matrix

--- R/rvine_structure.R
+++ R/rvine_structure.R
@@ -219,6 +219,7 @@ plot.rvine_structure <- function(x, tree = 1, edge_labels = FALSE, ...) {
 #' mat <- rbind(c(1, 1, 1), c(2, 2, 0), c(3, 0, 0))
 #' plot(rvine_matrix(mat))
 #' plot(rvine_matrix_sim(5))
+#' @export
 plot.rvine_matrix <- function(x, tree = 1, edge_labels = FALSE, ...) {
   plot(as_rvine_structure(x), tree = tree, edge_labels = edge_labels)
 }
